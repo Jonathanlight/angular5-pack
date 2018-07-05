33f27d7a4a75ff0d6291060b273f36a5822e4479
jkablan <jkablan@kernix.com>
2018-07-05 18:24:30 +0200

    create app angular 6

---
+++ main/README.md
@@ -0,0 +1,9 @@
+# Angular Material Template
+
+> Material design template for Angular apps, built with Angular CLI.
+
+```Shell
+npm install
+
+ng serve
+```
---
+++ main/src/assets/js/init.js
@@ -0,0 +1,37 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+    $('.carousel').carousel();
+
+    $('.collapsible').collapsible();
+
+    $('.dropdown-trigger').dropdown();
+
+    $('.tap-target').tapTarget();
+
+    $('.materialboxed').materialbox();
+
+    $('.modal').modal();
+
+    $('.parallax').parallax();
+
+    $('.pushpin').pushpin();
+
+    $('.scrollspy').scrollSpy();
+
+    $('.sidenav').sidenav();
+
+    $('.tabs').tabs();
+
+    $('.tooltipped').tooltip();
+
+    $('.datepicker').datepicker();
+
+    $('select').formSelect();
+
+    $('input#input_text, textarea#textarea2').characterCounter();
+
+  });
+})(jQuery);
